93f70fdae5499d3a01db3c131fd52130c77a8d20
OrAlmog11 <1oralmog.1@gmail.com>
2025-04-24 16:10:51 +0300

    MEOWV2

--- app/src/main/java/com/example/oralmogproject/MainActivity.java
+++ app/src/main/java/com/example/oralmogproject/MainActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnPlayOnlineWhite, btnPlayOnlineBlack, btnPlayOffline;
-    private String colorwon;
+    private String colorwon = "None";
     private int white_win_count = 0;
     private int black_win_count = 0;
 
@@ -43,19 +43,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         if (result.getResultCode() == RESULT_OK) {
                             Intent data = result.getData();
                             colorwon = data.getStringExtra("ColorWin");
+
+                        }
+                        if(colorwon.equals("White")){
+                            white_win_count++;
+                            Toast.makeText(MainActivity.this, "White won! White have " + white_win_count + " wins!", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(colorwon.equals("Black")){
+                            black_win_count++;
+                            Toast.makeText(MainActivity.this, "Black won! Black have " + black_win_count + " wins!", Toast.LENGTH_SHORT).show();
                         }
 
                     }
                 });
 
-        if(colorwon == "White"){
-            white_win_count++;
-            Toast.makeText(this, "White won! White have" + white_win_count + "wins!", Toast.LENGTH_SHORT).show();
-        }
-        else if(colorwon == "Black"){
-            black_win_count++;
-            Toast.makeText(this, "Black won! Black have" + black_win_count + "wins!", Toast.LENGTH_SHORT).show();
-        }
+
     }
 
 
